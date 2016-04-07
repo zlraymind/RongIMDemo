f2449f1807d93cf926fd6dff2122ac88214d0b3f
ZhangLei <zhanglei1@rongcloud.cn>
2016-04-07 19:22:44 +0800

    Add titlebar icons.

--- app/src/main/java/rong/im/demo/activity/MainActivity.java
+++ app/src/main/java/rong/im/demo/activity/MainActivity.java
@@ -2,14 +2,24 @@ package rong.im.demo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import rong.im.demo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView nor;
+    ImageView sel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_main);
+
+        nor = (ImageView) findViewById(R.id.titlebar_im_nor);
+        sel = (ImageView) findViewById(R.id.titlebar_im_sel);
+
+        nor.setAlpha(0.1f);
+        sel.setAlpha(0.9f);
     }
 }
